8a471a60e885f9de6821cdd7332d63683341d0af
Michiel Leyman <michiel.leyman@gmail.com>
2019-07-22 22:17:54 +0200

    Add context to track selected edition information

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,15 @@
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 import AppHeader from '../src/components/AppHeader';
+import SideNav from '../src/components/SideNav';
 import GlobalStyle from '../src/styles';
+import EditionContext from '../src/context/edition-context';
 
 class MyApp extends App {
+  state = {
+    selectedEdition: '2019'
+  };
+
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
 
@@ -14,8 +20,17 @@ class MyApp extends App {
     return { pageProps };
   }
 
+  setSelectedEdition = edition => {
+    this.setState({ selectedEdition: edition });
+  };
+
   render() {
     const { Component, pageProps } = this.props;
+
+    const editionContext = {
+      selectedEdition: this.state.selectedEdition,
+      setAuthenticatedUser: this.setSelectedEdition
+    };
 
     return (
       <Container>
@@ -23,8 +38,11 @@ class MyApp extends App {
         <Head>
           <title key="title">Dashboard | open Summer of code 2019</title>
         </Head>
-        <AppHeader />
-        <Component {...pageProps} />
+        <EditionContext.Provider value={editionContext}>
+          <AppHeader />
+          <SideNav />
+          <Component {...pageProps} />
+        </EditionContext.Provider>
       </Container>
     );
   }
